12fe2a06ab6a5f5811a10f7b760b96b0cac4ba95
Iliyan-0512 <imanov05@abv.bg>
2025-01-16 23:40:18 +0200

    add file sql query

---
+++ LibraryProject/sampleCodeForTable.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE Loans (
+    LoanID INT PRIMARY KEY IDENTITY(1,1),
+    BookID INT NOT NULL,
+    MemberID INT NOT NULL,
+    LoanDate DATE DEFAULT GETDATE(),
+    ReturnDate DATE,
+    Status NVARCHAR(20) DEFAULT 'Borrowed',
+    CONSTRAINT FK_Loans_Books FOREIGN KEY (BookID) REFERENCES Books(BookID),
+    CONSTRAINT FK_Loans_Members FOREIGN KEY (MemberID) REFERENCES Members(MemberID)
+);
